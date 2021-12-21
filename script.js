0643d57ed9b5dc48cb8d63879eda31751e8f0fbb
andrius-siup <andrius.siupinys@gmail.com>
2021-12-21 18:55:42 +0000

    Challenge - 1: restore to the default all game when player click on the again btn

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ console.log(document.querySelector('.guess').value);
 */
 
 // Random number between 0 and 20
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+// document.querySelector('.number').textContent = secretNumber;
 
 // select el .guess and add function click and print to console
 document.querySelector('.check').addEventListener('click', function () {
@@ -29,9 +29,11 @@ document.querySelector('.check').addEventListener('click', function () {
   // if no input number
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
@@ -61,4 +63,30 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+/*
+Coding Challenge #1
+Implement a game rest functionality, so that the player can make a new guess!
+Your tasks:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the 'score' and
+'secretNumber' variables
+3. Restore the initial conditions of the message, number, score and guess input
+fields
+4. Also restore the original background color (#222) and number width (15rem)
+*/
+document.querySelector('.again').addEventListener('click', function () {
+  // restore secret number
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = ''; // Guess box empty
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+
+  console.log('clicked again btn');
 });
