fa606b31ed3caca05db8faccb170889b7dc13e96
andrius-siup <andrius.siupinys@gmail.com>
2021-12-21 13:25:49 +0000

    Select element, and create function that when click btn the input value will be printed to console

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 // prt to console - select el class - than text content
 console.log(document.querySelector('.message').textContent);
 
@@ -12,3 +13,7 @@ document.querySelector('.score').textContent = 10;
 // can't be textContent as this class is input element - .value
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+})
