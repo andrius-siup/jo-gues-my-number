7b54761d82c11244d496ef9a2f6fbf8a0e9ade0c
andrius-siup <andrius.siupinys@gmail.com>
2021-12-21 14:54:50 +0000

    Updated - displayed message for 'Lost game' when the guess number is above the secret number - stoped game

--- script.js
+++ script.js
@@ -32,12 +32,13 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
   } else if (guess > secretNumber) {
-    if (score > 0) {
+    if (score > 1) {
       document.querySelector('.message').textContent = '👇 Too High!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = '🎃 You lost the game!';
+      document.querySelector('.score').textContent = 0;
     }
 
   } else if (guess < secretNumber) {
