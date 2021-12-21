747178d4c8b40eea254cd2d6763a5e47ad1905c4
andrius-siup <andrius.siupinys@gmail.com>
2021-12-21 13:10:45 +0000

    Replaced text content in the message

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict';
+// prt to console - select el class - than text content
+console.log(document.querySelector('.message').textContent);
+
+// Select the class and replace text content
+document.querySelector('.message').textContent = '🎈 Correct Number!';
