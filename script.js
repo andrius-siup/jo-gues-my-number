66d338fa4a977237bbb37e36e664b8e4b4288336
andrius-siup <andrius.siupinys@gmail.com>
2021-12-21 15:15:32 +0000

    Changed the background color into green color and increased a width of secret number when the player win the game

--- script.js
+++ script.js
@@ -29,10 +29,20 @@ document.querySelector('.check').addEventListener('click', function () {
   // if no input number
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
+
   } else if (guess > secretNumber) {
+
     if (score > 1) {
+
       document.querySelector('.message').textContent = '👇 Too High!';
       score--;
       document.querySelector('.score').textContent = score;
@@ -40,7 +50,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '🎃 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
-
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '👆 Too Low!';
