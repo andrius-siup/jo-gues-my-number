2f02c6f22141bceda5a23f835b1a49d9480cccff
andrius-siup <andrius.siupinys@gmail.com>
2021-12-21 13:20:14 +0000

    Slected classes and manipulated data

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ console.log(document.querySelector('.message').textContent);
 
 // Select the class and replace text content
 document.querySelector('.message').textContent = '🎈 Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+// can't be textContent as this class is input element - .value
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
