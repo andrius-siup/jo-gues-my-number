aac05f2c7c127cdcb2937db5d8a13c6e456cb510
andrius-siup <andrius.siupinys@gmail.com>
2021-12-21 14:58:07 +0000

    Stoped the game and displayed message than guess number was below the secret number and score is 0

--- script.js
+++ script.js
@@ -42,8 +42,13 @@ document.querySelector('.check').addEventListener('click', function () {
     }
 
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '👆 Too Low!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '👆 Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🎃 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
