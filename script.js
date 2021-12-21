e8623f60cdb5bcb7b693780a71e44cef56afa52a
andrius-siup <andrius.siupinys@gmail.com>
2021-12-21 19:45:50 +0000

    Refactoring code - reduced repeated code

--- script.js
+++ script.js
@@ -45,24 +45,9 @@ document.querySelector('.check').addEventListener('click', function () {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-
-    // When guess is too high
-
-  } else if (guess > secretNumber) {
-
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-
-      document.querySelector('.message').textContent = '👇 Too High!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '🎃 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-    // When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '👆 Too Low!';
+      document.querySelector('.message').textContent = guess > secretNumber ? '👇 Too High!' : '👆 Too Low!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
@@ -70,6 +55,30 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+  //   // When guess is too high
+  // } else if (guess > secretNumber) {
+
+  //   if (score > 1) {
+
+  //     document.querySelector('.message').textContent = '👇 Too High!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '🎃 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  //   // When guess is too low
+  // } else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '👆 Too Low!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '🎃 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  //}
+
 });
 
 /*
